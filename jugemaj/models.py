73b0ac8cb2ed9dad3cf701c4cf3f88c3c95229ae
Guilhem Saurel <guilhem.saurel@gmail.com>
2017-06-09 22:42:37 +0200

    update ranking system, fix #1

--- jugemaj/models.py
+++ jugemaj/models.py
@@ -1,4 +1,5 @@
 from enum import IntEnum
+from functools import cmp_to_key
 
 from django.db import models
 from django.conf import settings
@@ -11,6 +12,33 @@ CHOICES = IntEnum("choix", "Super Bien OK Passable Insuffisant Nul")
 
 def enum_to_choices(enum):
     return ((item.value, item.name) for item in list(enum))
+
+
+def sort_candidates(a, b):
+    """
+    a & b are the majority gauges of the candidates A & B:
+    (p_A, α_A, q_A) & (p_B, α_B, q_B)
+    where
+    - α is the majority grade;
+    - p the percentage of votes strictly above α;
+    - q the percentage of votes strictly below α.
+    thus, A > B ⇔ α_A > α_B || (α_A == α_B && (p_A > max(q_A, p_B, q_B) || q_B > max(p_A, q_A, p_B)))
+    (eq. 2, p.11 of the second article in README.md)
+    """
+    a, b = a.majority_gauge(), b.majority_gauge()
+    if a[1] > b[1]:
+        return 1
+    if b[1] > a[1]:
+        return -1
+    if a[0] > max(a[2], b[0], b[2]):
+        return 1
+    if b[0] > max(b[2], a[0], a[2]):
+        return -1
+    if b[2] > max(a[0], a[2], b[0]):
+        return 1
+    if a[2] > max(b[0], b[2], a[0]):
+        return -1
+    return 0
 
 
 class TimeStampedModel(models.Model):
@@ -36,7 +64,7 @@ class Election(TimeStampedModel):
         return reverse('jugemaj:election', kwargs={'slug': self.slug})
 
     def results(self):
-        return sorted(self.candidate_set.all(), key=lambda c: c.mentions())
+        return sorted(self.candidate_set.all(), key=cmp_to_key(sort_candidates))
 
 
 class Candidate(TimeStampedModel):
@@ -49,15 +77,13 @@ class Candidate(TimeStampedModel):
     def get_absolute_url(self):
         return self.election.get_absolute_url()
 
-    def mentions(self, exclude=None):
-        if exclude is None:
-            exclude = []
-        votes = self.vote_set.exclude(choice__in=exclude)
-        count = votes.count()
+    def majority_gauge(self):
+        count = self.vote_set.count()
         if not count:
-            return []
-        mention = [votes.order_by('choice')[count // 2].choice]
-        return mention + self.mentions(exclude=exclude + mention)
+            return (0, 10, 0)
+        mention = [self.vote_set.order_by('choice')[count // 2].choice]
+        return (self.vote_set.filter(choice_gt=mention).count() / count, mention,
+                self.vote_set.filter(choice_lt=mention).count() / count)
 
     def votes(self):
         count = self.vote_set.count()
