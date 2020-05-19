e1c487af94f8448c529c3bbfada18811dab1d4df
Guilhem Saurel <guilhem.saurel@laas.fr>
2020-05-19 20:38:11 +0200

    use setuptools_scm

--- setup.py
+++ setup.py
@@ -1,36 +1,48 @@
-import os
+#!/usr/bin/env python
+"""Packaging configuration."""
 
-from setuptools import setup
+import os
+import re
+
+from setuptools import setup  # type: ignore
 
 with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
     README = readme.read()
 
-with open(os.path.join(os.path.dirname(__file__), 'requirements.in')) as requirements:
-    REQUIREMENTS = [req.split('#egg=')[1] if '#egg=' in req else req for req in requirements.readlines()]
+with open(os.path.join(os.path.dirname(__file__), 'Pipfile')) as pipfile:
+    content = pipfile.read()
+    REQUIREMENTS = re.findall(r'''\n *['"]?([\w-]*)['"]? *=''', content.split('packages]')[1])
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-jugemaj',
-    version='1.0.0',
     packages=['jugemaj'],
     install_requires=REQUIREMENTS,
     include_package_data=True,
-    license='GPL License',
+    exclude_package_data={'': ['*.orig', '*.pyc']},
+    license='BSD',
     description='A Django app for the Majority Judgment voting system',
     long_description=README,
-    url='https://saurel.me/',
+    long_description_content_type="text/markdown",
+    url='https://github.com/nim65s/django-jugemaj',
     author='Guilhem Saurel',
     author_email='webmaster@saurel.me',
+    python_requires='>=3.6',
+    zip_safe=False,
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
