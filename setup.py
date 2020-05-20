b6a71cfccfa01f223aa5fb47b19b19090c980cca
Guilhem Saurel <guilhem.saurel@laas.fr>
2020-05-20 16:42:47 +0200

    setup.py: use poetry

--- setup.py
+++ setup.py
@@ -1,49 +1,38 @@
 #!/usr/bin/env python
-"""Packaging configuration."""
+"""Packaging configuration.
+
+This file actually doesn't contain any info. Everything is exctracted from pyproject.toml or git.
+"""
 
 import os
-import re
 
+from poetry.semver import parse_constraint  # type: ignore
+from poetry.utils.toml_file import TomlFile  # type: ignore
 from setuptools import setup  # type: ignore
-
-with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
-    README = readme.read()
-
-with open(os.path.join(os.path.dirname(__file__), 'Pipfile')) as pipfile:
-    content = pipfile.read()
-    REQUIREMENTS = re.findall(r'''\n *['"]?([\w-]*)['"]? *=''', content.split('packages]')[1])
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
-setup(
-    name='django-jugemaj',
-    packages=['jugemaj'],
-    install_requires=REQUIREMENTS,
-    include_package_data=True,
-    exclude_package_data={'': ['*.orig', '*.pyc']},
-    license='BSD',
-    description='A Django app for the Majority Judgment voting system',
-    long_description=README,
-    long_description_content_type="text/markdown",
-    url='https://github.com/nim65s/django-jugemaj',
-    author='Guilhem Saurel',
-    author_email='webmaster@saurel.me',
-    python_requires='>=3.6',
-    zip_safe=False,
-    use_scm_version=True,
-    setup_requires=['setuptools_scm'],
-    classifiers=[
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Topic :: Internet :: WWW/HTTP',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-    ],
-)
+# Extract infos from pyproject.toml
+# waiting for a better solution in https://github.com/python-poetry/poetry/issues/761
+POETRY = TomlFile('pyproject.toml').read()['tool']['poetry']
+with open(POETRY['readme']) as readme:
+    README = readme.read()
+
+setup(name=POETRY['name'],
+      packages=[pkg['include'] for pkg in POETRY['packages']],
+      install_requires=[f'{k}{parse_constraint(v)}' for k, v in POETRY['dependencies'].items() if k != 'python'],
+      include_package_data=True,
+      exclude_package_data={'': ['*.orig', '*.pyc']},
+      license=POETRY['license'],
+      description=POETRY['description'],
+      long_description=README,
+      long_description_content_type="text/markdown",
+      url=POETRY['homepage'],
+      author=POETRY['authors'][0].split('<')[0].strip(),
+      author_email=POETRY['authors'][0].split('<')[1][:-1],
+      python_requires=str(parse_constraint(POETRY['dependencies']['python'])),
+      zip_safe=False,
+      use_scm_version=True,
+      setup_requires=['setuptools_scm'],
+      classifiers=POETRY['classifiers'])
