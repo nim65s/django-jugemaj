d7f10773fde00b02fffc6ec5b215124fab92676e
Guilhem Saurel <guilhem.saurel@laas.fr>
2020-05-21 16:14:27 +0200

    __version_info__

--- jugemaj/__init__.py
+++ jugemaj/__init__.py
@@ -3,5 +3,6 @@
 try:
     from importlib.metadata import version
     __version__ = version('django_jugemaj')
+    __version_info__ = tuple(int(i) for i in __version__.split('.'))
 except ModuleNotFoundError:  # Python < 3.7
     pass
