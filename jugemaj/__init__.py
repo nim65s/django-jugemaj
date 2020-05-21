5bd2e530bba407cc9e559b047b1408471208df5e
Guilhem Saurel <guilhem.saurel@laas.fr>
2020-05-21 16:16:29 +0200

    comment

--- jugemaj/__init__.py
+++ jugemaj/__init__.py
@@ -1,6 +1,7 @@
 """Main module entrance."""
 
 try:
+    # Waiting for something better in https://github.com/python-poetry/poetry/issues/144
     from importlib.metadata import version
     __version__ = version('django_jugemaj')
     __version_info__ = tuple(int(i) for i in __version__.split('.'))
